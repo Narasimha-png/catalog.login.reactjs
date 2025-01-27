af40944f8aa440d51ed799663faa23a752855adb
Narasimha-png <narasimhas9490@gmail.com>
2025-01-27 12:07:34 +0530

    Login FIX

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -28,7 +28,7 @@ const LoginCard = () =>{
     const HandleSignIn = async ()=>{
         const body = JSON.stringify(FormData);
 
-        fetch("http://127.0.0.1:5000/api/v1/career/user/login", {
+        fetch("https://loginmiddilewarenodejs-production.up.railway.app/api/v1/career/user/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
